47c230b274ce6f167af0214dadc194ccdd4b2d7d
Pritam Shrungare <55679655+PritamShrungare@users.noreply.github.com>
2021-03-21 17:55:02 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar --> 
+      <nav class="navbar navbar-expand-md navbar-light bg-light">
+
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="viewuser.php">View All Customers</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php">Transfer Money</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php">Transaction History</a>
+              </li>
+          </div>
+       </nav>
